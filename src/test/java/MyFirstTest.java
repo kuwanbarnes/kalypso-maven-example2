c911810e60f0d123243903635e025f73361cb900
kuwan barnes <Kuwan.barne@gmail.com>
2021-02-14 19:22:41 -0600

    junit

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,46 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MyFirstTest {
+    @Test
+    public void testIfCompanyNameIsEquals() {
+        String expected = "Codeup";
+        String actual = "Codeup";
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testIfArraysListAreDifferent() {
+        List<String> languages = new ArrayList<>();
+        List<String> moreLanguages = new ArrayList<>();
+
+        assertNotSame(languages, moreLanguages);
+
+    }
+    @Test
+    public void testIfArrayEquals() {
+        int[] numbers = {1, 2, 3};
+        int[] otherNumbers = new int[3];
+        otherNumbers[0] = 1;
+        otherNumbers[1] = 2;
+        otherNumbers[2] = 3;
+
+        assertArrayEquals(numbers, otherNumbers);
+    }
+    @Test
+    public void testIflanguageContains() {
+
+        String language = "PHP";
+//        language.contains("H"); // use assertTrue for this statement
+//        language.contains("J"); // use assertFalse for this statement
+
+        assertTrue(language, true);
+        assertTrue(language.contains("H"));
+        assertFalse(language.contains("J"));
+    }
+
+}
